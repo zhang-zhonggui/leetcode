264c09b017438646badb3b1a288ab1096558c1c2
张中贵 <2521250151@qq.com>
2023-07-31 11:33:30 +0800

    日常刷题

---
+++ src/main/java/com/zzg/leetcode/utils/ListNode.java
@@ -0,0 +1,22 @@
+package com.zzg.leetcode.utils;
+
+
+/**
+ * Definition for singly-linked list.
+ */
+
+public class ListNode {
+    int val;
+
+    ListNode next;
+    ListNode() {
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
