3c59a511541942bf2a68a7981ba9e56d736d73ea
张中贵 <2521250151@qq.com>
2023-10-18 10:19:05 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day_2023_10/test1/Solution.java
@@ -0,0 +1,15 @@
+package com.zzg.leetcode.day_2023_10.test1;
+
+import java.util.Arrays;
+
+/**
+ * 268. 丢失的数字 https://leetcode.cn/problems/missing-number/description/
+ */
+class Solution {
+    public int missingNumber(int[] nums) {
+        // 计算数组nums的和
+        int sum = Arrays.stream(nums).sum();
+        // 返回数组nums的长度乘以数组nums的长度加1再除以2减去数组nums的和
+        return nums.length * (nums.length + 1) / 2 - sum;
+    }
+}
