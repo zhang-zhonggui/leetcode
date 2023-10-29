8b9115566065185d33a07d492e500a7388fdad4d
张中贵 <2521250151@qq.com>
2023-10-29 21:24:30 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day_2023_10/test7/Solution.java
@@ -0,0 +1,21 @@
+package com.zzg.leetcode.day_2023_10.test7;
+
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        if (citations.length == 1){
+            return 1;
+        }
+        Arrays.sort(citations);
+        int mid = (citations.length + 1) / 2 -1;
+        return citations[mid];
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr ={100};
+        int i = new Solution().hIndex(arr);
+        System.out.println(i);
+    }
+}
