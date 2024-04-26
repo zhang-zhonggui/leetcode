3745f7d547e91dd64c01c7f31d22faa45fd8943e
张中贵 <2521250151@qq.com>
2024-04-26 23:06:25 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/mianshi/Day3.java
@@ -0,0 +1,16 @@
+package com.zzg.leetcode.mianshi;
+
+import java.util.ArrayList;
+
+public class Day3 {
+    public static void main(String[] args) {
+        ArrayList<Student> list = new ArrayList<>();
+        list.add(new Student("张三", 18));
+        list.add(new Student("李四", 17));
+        list.add(new Student("王五", 16));
+        //使用jdk8的stream求年龄平均值
+        double avgAge = list.stream().mapToInt(Student::getAge).average().getAsDouble();
+
+
+    }
+}
