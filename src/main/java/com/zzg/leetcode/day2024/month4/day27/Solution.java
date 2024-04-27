cb86103fbaa0013aa6f020b6d7b6d79419dfcf44
张中贵 <2521250151@qq.com>
2024-04-27 23:53:49 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month4/day27/Solution.java
@@ -0,0 +1,25 @@
+package com.zzg.leetcode.day2024.month4.day27;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution {
+    public char repeatedCharacter(String s) {
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (list.contains(s.charAt(i))){
+                return s.charAt(i);
+            }
+            list.add(s.charAt(i));
+        }
+        return ' ';
+    }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> a = Arrays.stream(nums1).boxed().collect(Collectors.toSet());
+        Set<Integer> b = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
+        a.retainAll(b);
+        return a.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
