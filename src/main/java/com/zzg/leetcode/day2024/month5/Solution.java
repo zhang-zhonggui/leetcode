8941c8b0646695819fdc8dd6ee802f2a36db467b
张中贵 <2521250151@qq.com>
2024-05-05 14:37:52 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month5/Solution.java
@@ -0,0 +1,34 @@
+package com.zzg.leetcode.day2024.month5;
+
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] res = new int[n];
+        if(k == 0) {
+            return res;
+        } else if(k > 0) {
+            for(int i = 0; i < n; i++) {
+                int idx = i + 1;
+                int cnt = 0;
+                for(int j = 0; j < k; j++) {
+                    if(idx >= n) idx = 0;
+                    cnt += code[idx];
+                    idx++;
+                }
+                res[i] = cnt;
+            }
+        } else {
+            for(int i = 0; i < n; i++) {
+                int idx = i - 1;
+                int cnt = 0;
+                for(int j = 0; j < -k; j++) {
+                    if(idx < 0) idx = n - 1;
+                    cnt += code[idx];
+                    idx--;
+                }
+                res[i] = cnt;
+            }
+        }
+        return res;
+    }
+}
