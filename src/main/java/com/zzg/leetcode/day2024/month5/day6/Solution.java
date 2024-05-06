b1cef565889367154f82f7080a2c7cb7c966b5dd
张中贵 <2521250151@qq.com>
2024-05-06 22:41:25 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month5/day6/Solution.java
@@ -0,0 +1,52 @@
+package com.zzg.leetcode.day2024.month5.day6;
+
+import com.zzg.leetcode.utils.ListNode;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = ' ';
+            temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] array = "hello".toCharArray();
+        solution.reverseString(array);
+        System.out.println(array);
+    }
+
+
+}
