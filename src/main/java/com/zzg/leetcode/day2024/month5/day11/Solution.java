56bf280fba06d82935a484d661217d283c471043
张中贵 <2521250151@qq.com>
2024-05-11 22:48:25 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month5/day11/Solution.java
@@ -0,0 +1,18 @@
+package com.zzg.leetcode.day2024.month5.day11;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (int i = 0; i < jewels.length(); i++) {
+            for (int j = 0; j < stones.length(); j++) {
+                if (jewels.charAt(i) == stones.charAt(j)) {
+                    ++count;
+                }
+            }
+
+
+        }
+        return count;
+
+    }
+}
