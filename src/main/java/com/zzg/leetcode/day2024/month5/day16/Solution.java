632e53c8e51b7241f75fdf9cbb4d45c4aca3c637
张中贵 <2521250151@qq.com>
2024-05-16 12:09:13 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month5/day16/Solution.java
@@ -0,0 +1,11 @@
+package com.zzg.leetcode.day2024.month5.day16;
+
+import java.util.Arrays;
+
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+
+        int sum = Arrays.stream(prices).sorted().limit(2).sum();
+        return money >= sum ? money - sum : money;
+    }
+}
---
+++ src/main/java/com/zzg/leetcode/day2024/month5/day16/Solution1.java
@@ -0,0 +1,13 @@
+package com.zzg.leetcode.day2024.month5.day16;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution1 {
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate localDate = LocalDate.of(year, month, day);
+        // 获取英文全称
+        return localDate.getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, Locale.ENGLISH);
+    }
+}
