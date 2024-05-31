64d36465adee53a814ce41fa4bc5e6fa51efbd49
张中贵 <2521250151@qq.com>
2024-05-31 18:30:01 +0800

    今天又是快乐的一天

--- src/main/java/com/zzg/leetcode/day2024/month5/day30/Solution.java
+++ src/main/java/com/zzg/leetcode/day2024/month5/day30/Solution.java
@@ -3,7 +3,6 @@ package com.zzg.leetcode.day2024.month5.day30;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Set;
 
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
@@ -18,28 +17,16 @@ class Solution {
 
         }
         ints[0] = map.entrySet().stream().filter(e -> e.getValue() > 1).map(e -> e.getKey()).findFirst().orElse(0);
-        Set<Integer> set = map.keySet();
-        ints[1] = findDiscontinuity(set, count);
+        int sum = map.keySet().stream().mapToInt(Integer::intValue).sum();
+        System.out.println(count);
+        System.out.println(sum);
+        ints[1] = count * (count + 1) / 2 - sum;
         return ints;
-
     }
 
     public static void main(String[] args) {
         int[][] grid = new int[][]{{1, 3}, {2, 2}};
         int[] missingAndRepeatedValues = new Solution().findMissingAndRepeatedValues(grid);
         System.out.println(Arrays.toString(missingAndRepeatedValues));
-    }
-
-    public static int findDiscontinuity(Set<Integer> set, int n) {
-        // 从 1 开始遍历到 n
-        for (int i = 1; i <= n; i++) {
-            // 如果 Set 中不包含当前数字，则说明该数字缺失
-            if (!set.contains(i)) {
-                return i;
-            }
-        }
-
-        // 如果 Set 中包含了 1~n 中的所有数字，则返回 0
-        return 0;
     }
 }
