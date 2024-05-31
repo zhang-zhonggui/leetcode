c38f9bd4e6200c4f9bf3a647d3f2d28ee4c7bf60
张中贵 <2521250151@qq.com>
2024-05-31 19:57:56 +0800

    今天又是快乐的一天

--- src/main/java/com/zzg/leetcode/day2024/month5/day30/Solution.java
+++ src/main/java/com/zzg/leetcode/day2024/month5/day30/Solution.java
@@ -9,17 +9,19 @@ class Solution {
         int[] ints = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
+        //记录二级矩阵出现的次数,key是矩阵中的值,value是出现次数
         for (int[] arr : grid) {
             for (int i : arr) {
+                //计算矩阵容量
                 count++;
                 map.put(i, map.getOrDefault(i, 0) + 1);
             }
 
         }
+        //获取矩阵中出现二次的值,放入数组
         ints[0] = map.entrySet().stream().filter(e -> e.getValue() > 1).map(e -> e.getKey()).findFirst().orElse(0);
+        //计算矩阵中没出现的值,用矩阵容量 - 矩阵key的和
         int sum = map.keySet().stream().mapToInt(Integer::intValue).sum();
-        System.out.println(count);
-        System.out.println(sum);
         ints[1] = count * (count + 1) / 2 - sum;
         return ints;
     }
