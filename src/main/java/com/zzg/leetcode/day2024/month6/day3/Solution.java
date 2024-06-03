8a75ec04135419961f0bc317f33e66c06981a162
zzg <zzg.cn@hotmail.com>
2024-06-03 15:07:20 +0800

    今天又是快乐的一天

--- src/main/java/com/zzg/leetcode/day2024/month6/day3/Solution.java
+++ src/main/java/com/zzg/leetcode/day2024/month6/day3/Solution.java
@@ -14,9 +14,37 @@ class Solution {
         return arr;
     }
 
+    public int search(int[] nums, int target) {
+        int right = nums.length - 1, left = 0;
+        while (left <= right) {
+
+            int mid = (right - left) / 2 + left;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target > nums[mid]) {
+                left = mid + 1;
+            }
+
+            if (target < nums[mid]) {
+                right = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] data = solution.distributeCandies(10, 3);
         System.out.println(Arrays.toString(data));
+        int[] n = {-1, 0, 3, 5, 9, 12};
+        int search = solution.search(n, 2);
+        System.out.println(search);
+
     }
 }
