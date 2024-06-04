3ba0fdecf153b0abdf7aa1129771ef333575d3f8
zzg <zzg.cn@hotmail.com>
2024-06-04 10:43:10 +0800

    今天又是快乐的一天

--- src/main/java/com/zzg/leetcode/utils/TreeNode.java
+++ src/main/java/com/zzg/leetcode/utils/TreeNode.java
@@ -1,15 +1,21 @@
 package com.zzg.leetcode.utils;
 
-/**
- * @author zhang
- */
 
 public class TreeNode {
     int val;
     public TreeNode left;
     public TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
