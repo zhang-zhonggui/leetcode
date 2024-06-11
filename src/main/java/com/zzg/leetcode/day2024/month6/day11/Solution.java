7dfb841f47db253cc7916ed084153b67c400965e
zzg <zzg.cn@hotmail.com>
2024-06-11 22:54:15 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month6/day11/Solution.java
@@ -0,0 +1,13 @@
+package com.zzg.leetcode.day2024.month6.day11;
+
+import com.zzg.leetcode.utils.ListNode;
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null ){
+            return head;
+        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
