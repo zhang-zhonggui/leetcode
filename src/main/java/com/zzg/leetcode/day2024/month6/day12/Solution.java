f0fdc1d467c0036d7dec4668d64b7f4d929f0894
zzg <zzg.cn@hotmail.com>
2024-06-12 17:24:47 +0800

    今天又是快乐的一天

--- src/main/java/com/zzg/leetcode/day2024/month6/day12/Solution.java
+++ src/main/java/com/zzg/leetcode/day2024/month6/day12/Solution.java
@@ -1,5 +1,7 @@
 package com.zzg.leetcode.day2024.month6.day12;
 
+import com.zzg.leetcode.utils.ListNode;
+
 class Solution {
     public int accountBalanceAfterPurchase(int purchaseAmount) {
         int count;
@@ -11,7 +13,23 @@ class Solution {
         return 100 - count * 10;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().accountBalanceAfterPurchase(9));
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            // 保存当前节点的下一个节点
+            ListNode nextTemp = current.next;
+            // 将当前节点指向前一个节点，实现反转
+            current.next = prev;
+            // 前一个节点向后移动
+            prev = current;
+            // 当前节点向后移动
+            current = nextTemp;
+        }
+        // 最终prev会指向原链表的最后一个节点，即新链表的头节点
+        return prev;
     }
+
+
 }
