6b7b10ed94b12ee02fb3dfc863d63ff37f217a91
zzg <zzg.cn@hotmail.com>
2024-06-15 23:04:29 +0800

    今天又是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month6/day15/Solution.java
@@ -0,0 +1,9 @@
+package com.zzg.leetcode.day2024.month6.day15;
+
+import com.zzg.leetcode.utils.GuessGame;
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+
+    }
+}
