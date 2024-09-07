8d1ad606d11cf28d9f06931a22376f70067eb4b8
zzg <zzg.cn@hotmail.com>
2024-09-07 11:17:08 +0800

    今天是快乐的一天

---
+++ src/main/java/com/zzg/leetcode/day2024/month9/test7/Solution.java
@@ -0,0 +1,26 @@
+package com.zzg.leetcode.day2024.month9.test7;
+
+import com.zzg.leetcode.utils.ListNode;
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null)
+            return null;
+        ListNode tmp = new ListNode(0);
+        tmp.next = head;
+        dfs(tmp, n);
+        return tmp.next;
+    }
+
+    public int dfs(ListNode node, int n) {
+        if (node == null)
+            return 0;
+
+        int target = dfs(node.next, n);
+
+        if (target == n) {
+            node.next = node.next.next;
+        }
+        return target + 1;
+    }
+}
