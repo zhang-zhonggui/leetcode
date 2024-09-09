63b5f0020a318fa53b9bc296396e5c07e954beab
25212 <2521250151@qq.com>
2024-09-09 15:46:15 +0800

    代码优化

---
+++ src/main/java/com/zzg/leetcode/day2024/month9/test4/Solution.java
@@ -0,0 +1,18 @@
+package com.zzg.leetcode.day2024.month9.test4;
+
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid > x) {
+                right = mid - 1;
+            } else if ((long) mid * mid < x) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return right;
+    }
+}
