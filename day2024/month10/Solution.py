59cfca7033acceec914a66ce3682c8dda71a4518
zzg <zzg.cn@hotmail.com>
2024-10-12 23:28:17 +0800

    加入python

--- day2024/month10/Solution.py
+++ day2024/month10/Solution.py
@@ -1,2 +1,10 @@
 class Solution:
     def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        # 统计每个数字出现的次数
+        counter = Counter(nums)
+        ans = 0
+        # 遍历并计算
+        for x, count in counter.items():
+            if count == 2:
+                ans ^= x  # XOR操作
+        return ans
