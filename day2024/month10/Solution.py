a19a84f79ca510fb9576df6459751e0f5257f08a
zzg <zzg.cn@hotmail.com>
2024-10-12 23:28:02 +0800

    代码位置大调整，引入python做题

---
+++ day2024/month10/Solution.py
@@ -0,0 +1,2 @@
+class Solution:
+    def duplicateNumbersXOR(self, nums: List[int]) -> int:
---
+++ utils/Code.java
@@ -0,0 +1,8 @@
+package com.zzg.leetcode.utils;
+
+public class Code {
+    public static void main(String[] args) {
+        String str = "0信1息2信息3";
+        System.out.println(str);
+    }
+}
