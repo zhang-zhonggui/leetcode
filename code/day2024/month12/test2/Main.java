8a528f531ba5fb74f1770ecfe90cd9b37a29ccdb
zzg <zzg.cn@hotmail.com>
2024-12-18 18:33:08 +0800

    代码完善

---
+++ code/day2024/month12/test2/Main.java
@@ -0,0 +1,11 @@
+package day2024.month12.test2;
+
+public class Main {
+    public static Integer incr() {
+        return 1;
+    }
+
+    public static Integer decr() {
+        return 1;
+    }
+}
--- code/day2024/month12/test2/ThreadSafeCounter.java
+++ code/day2024/month12/test2/ThreadSafeCounter.java
@@ -1,25 +1,33 @@
 package day2024.month12.test2;
 
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadSafeCounter {
     private int i;
     // write your code
-    
+    private Lock lock;
     public ThreadSafeCounter() {
         this.i = 0;
         // write your code
+   lock = new ReentrantLock();
         
     }
     
     public void incr() {
         // write your code
+        lock.lock();
+        i = Main.incr();
+        lock.unlock();
         
     }
     
     public void decr() {
         // write your code
-        
+        lock.lock();
+        i = Main.decr();
+        lock.unlock();
     }
     
     public int getCount() {
